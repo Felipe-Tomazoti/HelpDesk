f5e3646dd0ff0211815efe9aed043e69ab07b2e1
Felipe-Tomazoti <felipecesar005@gmail.com>
2024-12-20 11:32:35 -0300

    tirando comentarios

--- src/main/java/com/felipe/helpdesk/config/exceptions/AuthenticationException.java
+++ src/main/java/com/felipe/helpdesk/config/exceptions/AuthenticationException.java
@@ -8,11 +8,9 @@ public class AuthenticationException extends StandardErrorConfig {
 
     public AuthenticationException(String path) {
         super(System.currentTimeMillis(), 401, "Unauthorized", "Token inválido ou não fornecido", path);
-        System.out.println("ERRO NO AUTHENTICATION1: ");
     }
 
     public static void handle(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        System.out.println("ERRO NO AUTHENTICATION1: ");
         AuthenticationException exception = new AuthenticationException(request.getRequestURI());
         exception.writeErrorResponse(response);
     }
